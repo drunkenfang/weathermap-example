54979136fbc48ad05bf5d564c4d25a1cadcc4993
drunkenfang <jungmin.fang@gmail.com>
2025-07-11 15:39:56 +0900

    thirdedit

---
+++ script.js
@@ -0,0 +1,47 @@
+const getCurrentWeather = (lat, lon) => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=e103a14bb85d1405438a6270052ad846&units=metric`;
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result)
+
+      const temp = document.querySelector(".temp") 
+      temp.innerText = `${result.main.temp}도`;
+
+      let weatherInfo;
+      const weather = document.querySelector(".weather") ;
+      switch(result.weather[0].main) {
+        case "Clear":
+          weatherInfo = "🌈맑음";
+      }
+      weather.innerText = weatherInfo;
+
+      let cityName;
+      const city = document.querySelector(".city");
+      switch(result.name) {
+        case "Jamwon-dong" :
+          cityName = "🌆 역삼동";
+      }
+      city.innerText = cityName;
+
+      const icon = document.querySelector(".icon");
+      icon.src = `https://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`
+    });
+};
+
+  
+const getPosition = (position) => {
+  const { latitude, longitude } = position.coords;
+  getCurrentWeather(latitude, longitude)
+};
+
+const errorHandle = (error) => {
+  console. error(error);
+}
+
+if("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(getPosition, errorHandle);
+} else {
+  console.log("geolocatio is not available");
+}
